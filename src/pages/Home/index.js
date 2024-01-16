f72128e192a07a075f5b9f4a675fed802f97123a
Yuri de Souza <yuris@unimedvaledocai.com.br>
2024-01-16 13:32:55 -0300

    Home page styles

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,20 +2,15 @@ import { Button, Grid, TextField } from '@mui/material';
 import React from 'react';
 import Logo from '../../components/Logo';
 import '../../css/pages/home.css';
-import { useTheme } from '@emotion/react';
 import { SearchIcon } from '../../defaults';
 
 
 const LOGO_FONT_SIZE = 80;
 const DEFAULT_SPACING = 56;
+const SMALL_SPACING = 32;
 
 
 const Home = () => {
-
-    const theme = useTheme();
-    //[ DEVELOPMENT ONLY ]
-    console.log('theme', theme);
-    console.log('theme.breakpoints.up("sm")', theme.breakpoints.up('sm'));
 
 
     return <Grid container className='home'>
@@ -23,14 +18,12 @@ const Home = () => {
             <div>
                 <Logo fontSize={LOGO_FONT_SIZE} />
             </div>
-            <Grid container item xs={12} style={{ gap: DEFAULT_SPACING }}>
-                {/* //[ DEVELOPMENT ONLY ] */}
-                {/* <Grid item xs={12} sm={10} style={{ maxWidth: (theme.breakpoints.up('sm')) ? `calc(83.333333% - ${DEFAULT_SPACING}px)` : '' }}> */}
-                <Grid item xs={12} sm={10}>
-                    <TextField fullWidth placeholder='Search' InputProps={{ startAdornment: <SearchIcon /> }} />
+            <Grid container item xs={12} style={{ gap: SMALL_SPACING }}>
+                <Grid item xs={12} sm={10} className='search_input-div'>
+                    <TextField fullWidth placeholder='Search' InputProps={{ startAdornment: <SearchIcon size={24} /> }} />
                 </Grid>
-                <Grid item xs={12} sm={2}>
-                    <Button fullWidth color='primary' variant='contained' size='large' style={{ height: DEFAULT_SPACING }} className='search-button'>Search</Button>
+                <Grid item xs={12} sm={2} className='search_button-div'>
+                    <Button fullWidth color='primary' variant='contained' className='search-button'>Search</Button>
                 </Grid>
             </Grid>
         </Grid>
